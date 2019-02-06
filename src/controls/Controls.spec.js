2377aed8a7d5914bb7876e30d4b89a50b08b3add
BryceEvans <evans.bryce@gmail.com>
2019-02-05 21:26:14 -0700

    problem fixed by moving to other file.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -28,18 +28,6 @@ describe('Controls Component', () => {
   });
 });
 
-// buttons' text changes to reflect the state the door will be in if clicked
-describe('button changes from close gate to open gate when clicked', () => {
-  it('open/close button text changes when clicked once', () => {
-    const { getByText } = render(<Controls locked={false} closed={false} />);
-    const button = getByText(/close gate/i);
-
-    fireEvent.click(button);
-    
-    getByText(/open gate/i);
-  })
-})
-
 // the closed toggle button is disabled if the gate is closed
 // describe('', () => {
 //   it('', () => {
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,12 +1,25 @@
 // Test away
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
+import Controls from '../controls/Controls';
 
 describe('Dahsboard Component', () => {
   it('displays dashboard component', () => {
     render(<Dashboard />);
+  })
+})
+
+// buttons' text changes to reflect the state the door will be in if clicked
+describe('button changes from close gate to open gate when clicked', () => {
+  it('open/close button text changes when clicked once', () => {
+    const { getByText } = render(<Controls locked={false} closed={false} />);
+    const button = getByText(/close gate/i);
+
+    fireEvent.click(button);
+    
+    getByText(/close gate/i);
   })
 })
